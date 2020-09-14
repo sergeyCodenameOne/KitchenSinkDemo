38560701868a65cab9d4b34ae51d127ddf89d2a6
sergey2065 <55790679+sergey2065@users.noreply.github.com>
2020-09-14 18:45:54 +0300

    Finished the DialogDemo
    Finished the DialogDemo and almost finished the Progress demo.

--- src/com/codename1/demos/kitchen/DialogDemo.java
+++ src/com/codename1/demos/kitchen/DialogDemo.java
@@ -6,7 +6,7 @@
  * published by the Free Software Foundation.  Codename One designates this
  * particular file as subject to the "Classpath" exception as provided
  * by Oracle in the LICENSE file that accompanied this code.
- *  
+ * 
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
@@ -22,16 +22,25 @@
  */
 package com.codename1.demos.kitchen;
 
+import com.codename1.components.InteractionDialog;
+import com.codename1.components.SpanLabel;
+import com.codename1.components.ToastBar;
+import com.codename1.ui.Button;
 import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Display;
 import com.codename1.ui.Form;
+import com.codename1.ui.Sheet;
+import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import static com.codename1.ui.util.Resources.getGlobalResources;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author serge
- */
+
 public class DialogDemo extends Demo {
+    boolean isInteractionDialogOpen = false;
+    private static List<ToastBar.Status> statusList = new ArrayList<>();
     
     public DialogDemo(Form parentForm) {
         init("Dialog", getGlobalResources().getImage("dialog-demo.png"), parentForm, "");
@@ -96,18 +105,119 @@ public class DialogDemo extends Demo {
     }
     
     private Container createInteractionDialogDemo(){
-        return new Container();
+        InteractionDialog dlg = new InteractionDialog("Header", new BorderLayout());
+        dlg.add(BorderLayout.CENTER, new SpanLabel("Dialog body"));
+        Button openClose = new Button("Opne/Close");
+        openClose.addActionListener((ee) ->{
+            if(isInteractionDialogOpen){ 
+                isInteractionDialogOpen = !isInteractionDialogOpen;
+                dlg.dispose();
+            }else {
+                isInteractionDialogOpen = !isInteractionDialogOpen;
+                dlg.show(0, Display.getInstance().getDisplayHeight() / 2, 0, 0);
+            }   
+            
+        });        
+        return BorderLayout.south(openClose);
     }
     
     private Container createDialogDemo(){
-        return new Container();    
+        final Button show = new Button("Show Dialog");
+        final Button showPopup = new Button("Show Popup");
+        show.addActionListener(e-> {
+            Dialog.show("Dialog Title", "This is the dialog body, it can contain anything...", "OK", "Cancel");
+        });
+        showPopup.addActionListener(e-> {
+            Dialog d = new Dialog("Popup Title");
+            SpanLabel label = new SpanLabel("This is the body of the popup", "WhiteBGLabel");
+            d.setLayout(new BorderLayout());
+            Container popupBody = BorderLayout.center(label);
+            popupBody.setUIID("WhiteContainer");
+            d.add(BorderLayout.CENTER, popupBody);
+            d.showPopupDialog(showPopup);
+
+        });
+        return BoxLayout.encloseY(show, showPopup);
     }
     
     private Container createSheetDemo(){
-        return new Container();
+        Button b = new Button("Open Sheet");
+        b.addActionListener(e->{
+            new MySheet(null).show();
+        });
+        return BorderLayout.north(b);
     }
     
     private Container createToastBarDemo(){
-        return new Container();
+        Button basic = new Button("Basic");
+        Button progress = new Button("Progress");
+        Button expires = new Button("Expires (after 3 seconds)");
+        Button delayed = new Button("Delayed (by 1 second)");
+        Button clear = new Button("Clear All");
+        
+        clear.addActionListener(e->{
+            for(ToastBar.Status currStatus : statusList){
+                currStatus.clear();
+            }
+            statusList.clear();
+        });
+        
+        basic.addActionListener(e -> {
+            ToastBar.Status status = ToastBar.getInstance().createStatus();
+            status.setMessage("Hello world");
+            statusList.add(status);
+            status.show();
+            //...  Some time later you must clear the status
+            // status.clear();
+        });
+
+        progress.addActionListener(e -> {
+            ToastBar.Status status = ToastBar.getInstance().createStatus();
+            status.setMessage("Hello world");
+            status.setShowProgressIndicator(true);
+            statusList.add(status);
+            status.show();
+            // ... Some time later you must clear it
+        });
+
+        expires.addActionListener(e -> {
+            ToastBar.Status status = ToastBar.getInstance().createStatus();
+            status.setMessage("Hello world");
+            status.setExpires(3000);  // only show the status for 3 seconds, then have it automatically clear
+            status.show();
+        });
+
+        delayed.addActionListener(e -> {
+            ToastBar.Status status = ToastBar.getInstance().createStatus();
+            status.setMessage("Hello world");
+            statusList.add(status);
+            status.showDelayed(1000); // Wait 1000 ms to show the status
+            // ... Some time later, clear the status... this may be before it shows at all
+        });
+        
+        return BoxLayout.encloseY(basic, progress, expires, delayed, clear);
+    }
+    
+    private class MySheet extends Sheet {
+        MySheet(Sheet parent) {
+            super(parent, "My Sheet");
+            setLayout(BoxLayout.y());
+            Button gotoSheet2 = new Button("Goto Sheet 2");
+            gotoSheet2.addActionListener(e->{
+                new MySheet2(this).show(300);
+            });
+            add(gotoSheet2);
+            add(new SpanLabel("Sheet body (you can add anything in here)"));
+        }
+    }
+    
+    private class MySheet2 extends Sheet {
+        MySheet2(Sheet parent) {
+            super(parent, "Sheet 2");
+            setLayout(BoxLayout.y());
+            setScrollableY(true);
+            add(new SpanLabel("Sheet 2 body"));
+            
+        }
     }
 }
--- src/com/codename1/demos/kitchen/ProgressDemo.java
+++ src/com/codename1/demos/kitchen/ProgressDemo.java
@@ -22,9 +22,41 @@
  */
 package com.codename1.demos.kitchen;
 
+import com.codename1.components.InfiniteProgress;
+import com.codename1.components.ScaleImageLabel;
+import com.codename1.components.SliderBridge;
+import com.codename1.components.SpanLabel;
+import com.codename1.io.ConnectionRequest;
+import static com.codename1.io.ConnectionRequest.fetchJSONAsync;
+import com.codename1.io.NetworkManager;
+import static com.codename1.io.Util.sleep;
+import com.codename1.ui.Button;
+import com.codename1.ui.CN;
+import static com.codename1.ui.CN.invokeAndBlock;
+import com.codename1.ui.CommonProgressAnimations.CircleProgress;
+import com.codename1.ui.CommonProgressAnimations.LoadingTextAnimation;
 import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Display;
+import com.codename1.ui.EncodedImage;
+import com.codename1.ui.Font;
+import com.codename1.ui.FontImage;
 import com.codename1.ui.Form;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.Slider;
+import com.codename1.ui.animations.CommonTransitions;
+import com.codename1.ui.geom.Dimension;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.plaf.Border;
+import com.codename1.ui.plaf.Style;
+import com.codename1.ui.plaf.UIManager;
 import static com.codename1.ui.util.Resources.getGlobalResources;
+import com.codename1.util.AsyncResource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -38,6 +70,122 @@ public class ProgressDemo extends Demo {
      
     @Override
     public Container createContentPane() {
-        return new Container();
+        Container demoContainer = new Container(new BoxLayout(BoxLayout.Y_AXIS), "DemoContainer");
+        demoContainer.setScrollableY(true);
+        ContentBuilder builder = ContentBuilder.getInstance();
+        
+        demoContainer.add(builder.createComponent(getGlobalResources().getImage("infinite-progress.png"),
+                                                    "Infinite Progess",
+                                                    "Shows a \"Washing Machine\" infinite",
+                                                    "progress indication animation, to customize the image "+
+                                                    "you can either use the infiniteImage theme constant or the setAnimation method. The image "+
+                                                    "is rotated automatically so don't use an animated image or anything like that as it would "+
+                                                    "fail with the rotation logic.",
+                                                    e->{
+                                                        showDemo("Infinite Progess", createInfiniteProgessDemo());
+                                                    }));
+        
+        demoContainer.add(builder.createComponent(getGlobalResources().getImage("slider.png"),
+                                                    "Slider",
+                                                    "The slider component serves both as a",
+                                                    "slider widget to allow users to select a value on a scale via touch/arrows and also to indicate progress. The slider "+
+                                                    "defaults to percentage display but can represent any positive set of values.",
+                                                    e->{
+                                                        showDemo("Slider", createSliderDemo());
+                                                    }));
+        
+        demoContainer.add(builder.createComponent(getGlobalResources().getImage("circle-animation.png"),
+                                                    "Circle Animation",
+                                                    "A CommonProgrssAnimations which shows",
+                                                    "radial coloring to show circular progress, like a Pac-Man",
+                                                    e->{
+                                                        showDemo("Circle Animation", createCircleAnimationDemo());
+                                                    }));
+        
+        demoContainer.add(builder.createComponent(getGlobalResources().getImage("text-loading-animation.png"),
+                                                    "Text Loading Animation",
+                                                    "A CommonProgressAnimations item used ",
+                                                    "to show the text is loading when we are fetching some text data from network/database",
+                                                    e->{
+                                                        showDemo("Text Loading Animation", createTextLoadAnimationDemo());
+                                                    }));
+        
+        return demoContainer;
+    }
+    
+    private Container createInfiniteProgessDemo(){
+        Dialog ip = new InfiniteProgress().showInfiniteBlocking();
+        invokeAndBlock(()->{
+            sleep(4000); // do some long operation here.
+            CN.callSerially(()-> ip.dispose());
+        });
+        InfiniteProgress prog = new InfiniteProgress();
+        prog.setAnimation(FontImage.createMaterial(FontImage.MATERIAL_INFO, UIManager.getInstance().getComponentStyle("DemoInfiniteProgress")));
+        return BoxLayout.encloseY(new InfiniteProgress(), prog);
+    }
+    
+    private Container createSliderDemo(){
+        Slider progress = new Slider();
+        Button download = new Button("Download");
+        Container demoContainer = BorderLayout.south(progress).
+                                    add(BorderLayout.NORTH, download);
+        
+        download.addActionListener((e) -> {
+            ConnectionRequest cr = new ConnectionRequest("https://www.codenameone.com/img/blog/new_icon.png", false);
+            SliderBridge.bindProgress(cr, progress);
+            NetworkManager.getInstance().addToQueueAndWait(cr);
+            if(cr.getResponseCode() == 200) {
+                demoContainer.add(BorderLayout.CENTER, new ScaleImageLabel(EncodedImage.create(cr.getResponseData())));
+                demoContainer.revalidate();
+            }
+        });
+        return demoContainer;
+    }
+    
+    private Container createCircleAnimationDemo(){
+        Label nameLabel = new Label("placeholder", "CenterAlignmentLabel");
+        Container demoContainer = BorderLayout.center(nameLabel);
+        // Replace the label by a CircleProgress to indicate that it is loading.
+        CircleProgress.markComponentLoading(nameLabel).setUIID("RedColor");
+
+        AsyncResource<Map<String,Object>> request = fetchJSONAsync("https://anapioficeandfire.com/api/characters/583");
+        request.ready(data -> {
+            nameLabel.setText((String)data.get("name"));
+
+            // Replace the progress with the nameLabel now that
+            // it is ready, using a fade transition
+            CircleProgress.markComponentReady(nameLabel, CommonTransitions.createFade(300));
+        });
+        return demoContainer;
+    }
+    
+    private Container createTextLoadAnimationDemo(){
+        SpanLabel profileText = new SpanLabel("placeholder", "CenterAlignmentLabel");
+
+        Container demoContainer = BorderLayout.center(profileText);
+        // Replace the label by a CircleProgress to indicate that it is loading.
+        LoadingTextAnimation.markComponentLoading(profileText);
+
+        AsyncResource<Map<String,Object>> request = fetchJSONAsync("https://anapioficeandfire.com/api/characters/583");
+        request.ready(data -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append("name: " + data.get("name") + "\n");
+            sb.append("gender: " + data.get("gender") + "\n");
+            sb.append("culture: " + data.get("culture") + "\n");
+            sb.append("born: " + data.get("born") + "\n");
+            List<String> aliases = (ArrayList<String>)data.get("aliases");
+            
+            sb.append("aliases: \n");
+            for (String alias : aliases){
+                sb.append(alias + "\n");
+            }
+
+            profileText.setText(sb.toString());
+
+            // Replace the progress with the nameLabel now that
+            // it is ready, using a fade transition
+            LoadingTextAnimation.markComponentReady(profileText, CommonTransitions.createFade(300));
+        });
+        return demoContainer;
     }
 }
